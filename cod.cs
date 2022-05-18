01ee85d1caa0c0499ae428c7273b2a9435525f32
dakeloon <94038117+dakeloon@users.noreply.github.com>
2022-05-18 18:59:37 +0700

    cod
    запуск бота, функия хелп, и русский с английским, пока задания по английскому показываются с багами

---
+++ cod.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Timers;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace DiscordBot
+{
+
+    class Program
+    {
+        DiscordSocketClient? Client;
+        System.Timers.Timer timer = new System.Timers.Timer() { Interval = 180000 };
+        static int task = 1;
+        static void Main(string[] args)
+        {
+
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public static int GenerateRandomOddNumber()
+        {
+            Random rnd = new Random();
+
+            int number = 0;
+            while (number % 2 == 0)
+            {
+                if (number % 2 != 0)
+                {
+                    return number;
+                }
+                else
+                {
+                    number = rnd.Next(1, 10);
+                }
+            }
+            return number;
+
+        }
+
+        private void SetTimer(SocketMessage msg)
+        {
+            timer.Start();
+            timer.Elapsed += (o, e) =>
+            {
+                EnglishTasks(msg);
+            };
+        }
+
+        private void StopTimer()
+        {
+            timer.Stop();
+        }
+
+        public static void EnglishTasks(SocketMessage msg)
+        {
+            string path = @"C:\Users\sg421\OneDrive\Desktop\bot.txt";
+            string line;
+            int LineNumber = 1;
+            int TaskNumber = GenerateRandomOddNumber();
+
+            StreamReader sr = new StreamReader(path);
+
+            line = sr.ReadLine();
+
+            while (line != null)
+            {
+                if (TaskNumber == LineNumber)
+                {
+                    msg.Channel.SendMessageAsync($"Задание {task}");
+                    ++task;
+                    msg.Channel.SendMessageAsync(line);
+                    line = sr.ReadLine();
+                    ++LineNumber;
+                    msg.Channel.SendMessageAsync(line);
+                    break;
+                }
+                line = sr.ReadLine();
+                ++LineNumber;
+            }
+            sr.Close();
+        }
+        private async Task MainAsync()
+        {
+            Client = new DiscordSocketClient();
+            Client.MessageReceived += CommandHandler;
+            Client.Log += Log;
+
+            var Token = "OTc2NDM1NDQ0NDk1NTgxMjI0.GbGaZy.K1HmtgHStGr-3y3NKsTLq-4LnaVSk_PBY7T95g";
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+
+            Console.ReadLine();
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task CommandHandler(SocketMessage msg)
+        {
+            if (!msg.Author.IsBot)
+            {
+                switch (msg.Content)
+                {
+                    case "!help":
+                        {
+                            msg.Channel.SendMessageAsync("1 - !рус\n2 - !англ");
+                            break;
+                        }
+                    case "!рус":
+                        {
+                            msg.Channel.SendMessageAsync($"Привет, {msg.Author}");
+                            break;
+                        }
+                    case "!англ":
+                        {
+                            EnglishTasks(msg);
+                            SetTimer(msg);
+                            break;
+                        }
+                    case "!стоп":
+                        {
+                            StopTimer();
+                            break;
+                        }
+                }
+            }
+            return Task.CompletedTask;
+
+        }
+
+
+    }
+}
